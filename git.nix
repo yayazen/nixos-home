84c47f2276ffb90a2b03f02a82c9b65e84d4c170
yayazen <yanis@mammar.xyz>
2025-08-11 18:08:17 +0200

    feat(git): add some nice aliases

--- git.nix
+++ git.nix
@@ -15,11 +15,23 @@ in
   home.packages = [ pkgs.ghq ];
 
   home.shellAliases = {
+    # cd to a cloned ghq repo
     gcd = ''
       repo="$(ghq list | ${pkgs.fzf}/bin/fzf \
             --reverse \
             --preview "CLICOLOR_FORCE=1 COLORTERM=truecolor ${pkgs.glow}/bin/glow --style=dark $(ghq root)/{1}/README.md 2>/dev/null")" \
-            && [ -n "$repo" ] && cd "$(ghq root)/$repo"'';
+        && [ -n "$repo" ] && cd "$(ghq root)/$repo"'';
+
+    # search and clone github repositories
+    ghcl = ''
+      echo | ${pkgs.fzf}/bin/fzf \
+        --reverse \
+        --prompt="Tab to search GH repos > " \
+        --preview "[ -n '{}' ] && gh repo view {} | CLICOLOR_FORCE=1 COLORTERM=truecolor ${pkgs.glow}/bin/glow --style=dark" \
+        --bind "ctrl-w:execute-silent([ -n '{}' ] && gh repo view {} -w)" \
+        --bind "tab:reload(gh search repos \$(echo {q}) --json fullName -q '.[].fullName')+clear-query" \
+        --bind "enter:become([ -n '{}' ] && ${pkgs.gum}/bin/gum confirm 'Clone {} ?' && gh repo view {} --json sshUrl -q '.sshUrl' | ghq get -u)"
+    '';
   };
 
   programs.git = {
@@ -49,6 +61,7 @@ in
     enable = true;
     gitCredentialHelper.enable = true;
     extensions = [
+      pkgs.gh-dash
       pkgs.gh-eco
     ];
     settings = {
